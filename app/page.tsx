56c6df364bace6d62f5f2efc43b0302b61492ee1
yhwa <yhwabillie93@gmail.com>
2024-07-15 16:14:27 +0900

    test: yarn classic build 시간

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <p>yarn classic</p>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
